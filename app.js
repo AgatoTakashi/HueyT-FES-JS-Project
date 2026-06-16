1fa2c8d3e629f43b2f01f958037d9424284bd4d6
Huey T <hueyathotmail@gmail.com>
2026-06-16 04:51:41 -0500

    card styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const apikey = "c706a2e0&s"
-const apiString = `http://www.omdbapi.com/?apikey=${apikey}&s=marvel`;
+const apiString = `http://www.omdbapi.com/?apikey=${apikey}&s=future`;
 const searchResultsEl = document.querySelector('.results-list');
 
 async function main () {
@@ -8,9 +8,10 @@ async function main () {
     console.log(resultsArr.Search)
     searchResultsEl.innerHTML = resultsArr.Search.map((movie) => 
         `<div class="card">
-            <img src="${movie.Poster}" alt="Movie Poster" class="movie__poster">
-            <h2 class="movie__title">${movie.Title}</h1>
-            <h3 class="movie__year">${movie.Year}</h2>
+            <img src="${movie.Poster}" alt="Movie Poster" class="movie__poster--img" height="444" width="300">
+            <h2 class="movie__title">${movie.Title}</h2>
+            <h3 class="movie__year">${movie.Year}</h3>
+            <p class="movie__id libre-barcode-128-regular">${movie.imdbID}</p>
         </div>`);
 }
 
