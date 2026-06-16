0dad283d3da3380a6ef7599d16dc98de7d289c23
Huey T <hueyathotmail@gmail.com>
2026-06-16 11:47:47 -0500

    sort filter added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const apikey = "c706a2e0&s"
 const searchResultsEl = document.querySelector('.results-list');
+let sortMethod = 'Sort';
 
 async function submitInput() {
     const inputElement = document.getElementById("userInput");
@@ -11,21 +12,46 @@ async function submitInput() {
     return renderSearch(userValue);
 }
 
-async function renderSearch (userValue) {
-    const apiString = `http://www.omdbapi.com/?apikey=${apikey}&s=${userValue}`;
-    const results = await fetch(`${apiString}`);
-    const resultsArr = await results.json();
-    console.log(resultsArr);
-    searchResultsEl.innerHTML = resultsArr.Search.map(movie => postHTML(movie));
+async function renderSearch(userValue) {
+    try {
+        const apiString = `http://www.omdbapi.com/?apikey=${apikey}&s=${userValue}`;
+        const results = await fetch(`${apiString}`);
+        const resultsArr = await results.json();
+
+        if (sortMethod === 'A_TO_Z') {
+            const filteredArr = resultsArr.Search.sort((a, b) => a.Title.localeCompare(b.Title));
+            searchResultsEl.innerHTML = filteredArr.map(movie => postHTML(movie));
+        }
+        else if (sortMethod === 'Z_TO_A') {
+            const filteredArr = resultsArr.Search.sort((a, b) => b.Title.localeCompare(a.Title));
+            searchResultsEl.innerHTML = filteredArr.map(movie => postHTML(movie));
+        }
+        else if (sortMethod === 'NEW_TO_OLD') {
+            const filteredArr = resultsArr.Search.sort((a, b) => b.Year.slice(0,4) - a.Year.slice(0,4));
+            searchResultsEl.innerHTML = filteredArr.map(movie => postHTML(movie));
+        }
+        else if (sortMethod === 'OLD_TO_NEW') {
+            const filteredArr = resultsArr.Search.sort((a, b) => a.Year.slice(0,4) - b.Year.slice(0,4));
+            searchResultsEl.innerHTML = filteredArr.map(movie => postHTML(movie));
+        }
+        else {
+            searchResultsEl.innerHTML = resultsArr.Search.map(movie => postHTML(movie));
+        }
+    } catch (err) {
+        alert('Movie not found!');
+        console.log(sortMethod);
+    }
 }
 
 function postHTML(movie) {
-    return `<div class="card">
+    return `
+        <div class="card">
             <img src="${movie.Poster}" alt="Movie Poster" class="movie__poster--img" height="444" width="300">
             <h2 class="movie__title">${movie.Title}</h2>
             <h3 class="movie__year">${movie.Year}</h3>
             <p class="movie__id libre-barcode-128-regular">${movie.imdbID}</p>
-        </div>`
+        </div>
+    `
 }
 
 const input = document.getElementById("userInput");
@@ -35,3 +61,9 @@ input.addEventListener("keydown", function (event) {
     submitInput();
   }
 });
+
+
+function sortMovies(event) {
+    sortMethod = event.target.value;
+    submitInput();
+}
